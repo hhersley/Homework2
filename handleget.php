93d4c3db02cc6f0b486f5344b4fe920228b24ecb
hhersley <89473870+hhersley@users.noreply.github.com>
2022-09-18 17:14:12 -0500

    Update handleget.php

--- handleget.php
+++ handleget.php
@@ -8,10 +8,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Post response</h1>
-   Hello<?php echo $_GET["name"]; ?><br>
-Your favorite color is: <?php echo $_GET["favorite color"]; ?><br>
-Your favorite weather is: <php echo $_GET["favorite weather"]; ?><br>
+    <h1>GET response</h1>
+   Hello <?php echo $_GET["name"]; ?><br>
+Your favorite color is: <?php echo $_GET["favoritecolor"]; ?><br>
+Your favorite weather is: <php echo $_GET["favoriteweather"]; ?><br>
    
   </body>
 </html>
