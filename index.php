5bdcf99ee486de08d5f753442d709ffc61f3d9ba
hhersley <89473870+hhersley@users.noreply.github.com>
2022-09-18 18:09:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,7 @@
 <html lang="en">
 <?php require_once('header.php'); ?>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Homework 2</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
+
     
     <h1> Find your dream Trip!</h1>
     <h1>Sign up for Rewards</h1>
