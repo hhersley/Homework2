319e67ffa80db2165ab6bf6219dbe0d54fad03b3
hhersley <89473870+hhersley@users.noreply.github.com>
2022-09-18 18:54:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,16 @@
     
     <h1> Find your dream Trip!</h1>
     <h1>Sign up for Rewards</h1>
-  
+  <h1>
     <form action="handlepost.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
-  
+  </h1>
     <h1>OR</h1>
       <h1>Check your coupon code below</h1>
-  
+  <h1>
           <form action="handleget.php" method="get">
 Coupon Code: <input type="text" name="name"><br>
 
@@ -23,7 +23,7 @@ Coupon Code: <input type="text" name="name"><br>
 <input type="submit">
 </form>
        
-    
+  </h1>
     
     <?php require_once('footer.php'); ?>
 
