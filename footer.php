d35b84678156fe73ddc60f47273be2be6c63c636
hhersley <89473870+hhersley@users.noreply.github.com>
2022-09-18 18:25:52 -0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+  <body>
+    <footer>
+      <p> follow us on our social media!
+      </p>
+      <a href="https://www.facebook.com/" class="fa fa-facebook"></a>
+
+    </footer>
+  </body>
