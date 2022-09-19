7968777f872e4a48b2b1f87e2e23c444d9c0821e
hhersley <89473870+hhersley@users.noreply.github.com>
2022-09-18 19:44:37 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -12,9 +12,12 @@
   <body>
     <h1>Post response</h1>
     Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-   
+Your email address is: <?php echo $_POST["email"]; ?><br>
+    <img src="https://media0.giphy.com/media/xUPGGDNsLvqsBOhuU0/giphy.gif?cid=ecf05e47td76ml2ubtsgi3y8vsq96449hsu7srtoi8yqyam4&rid=giphy.gif&ct=g" class="img-thumbnail" alt="...">
+    
+    
   </body>
+  
       <?php require_once('footer.php'); ?>
 
 </html>
