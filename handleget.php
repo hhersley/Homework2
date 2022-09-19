d6d9b4637123cb3baec51f1117233ad10fbfdd52
hhersley <89473870+hhersley@users.noreply.github.com>
2022-09-18 19:05:30 -0500

    Update handleget.php

--- handleget.php
+++ handleget.php
@@ -1,14 +1,7 @@
 <!doctype html>
 <html lang="en">
   <?php require_once('header.php'); ?>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Homework 2</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-       <link rel="stylesheet" href="StyleSheet2.css" />
 
-  </head>
   <body>
     <h1>GET response</h1>
    Your Coupon Code is <?php echo $_GET["name"]; ?> and it is worth 20% off of your next purchase!<br>
