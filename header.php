ed81c39de8ed4f481de5c2aeae2248fd73c75c67
hhersley <89473870+hhersley@users.noreply.github.com>
2022-09-18 19:55:18 -0500

    Update header.php

--- header.php
+++ header.php
@@ -22,17 +22,17 @@
           <a class="nav-link active" aria-current="page" href="https://homework2.hahersley.oucreate.com/#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="https://www.rottentomatoes.com/">Rotten Tomatoes</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Movie Theaters
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="https://www.cinemark.com/">Cinemark</a></li>
+            <li><a class="dropdown-item" href="https://www.amctheatres.com/">AMC</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="https://www.regmovies.com/">Regal</a></li>
           </ul>
         </li>
     
