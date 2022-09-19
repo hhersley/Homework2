e350d99e37e0e6fb75a20917020ae1de3b930dd6
hhersley <89473870+hhersley@users.noreply.github.com>
2022-09-18 19:40:19 -0500

    Update handleget.php

--- handleget.php
+++ handleget.php
@@ -7,7 +7,8 @@
    Your Coupon Code is <?php echo $_GET["name"]; ?> and it is worth 20% off of your next purchase!<br>
    
     <iframe src="https://giphy.com/embed/UHTfDj7UYiHlzjTW19" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/pauladaniotti-sale-paulaotti-paula-otti-UHTfDj7UYiHlzjTW19">via GIPHY</a></p>
-    
+    <img src="https://media0.giphy.com/media/UHTfDj7UYiHlzjTW19/giphy.gif?cid=ecf05e47xwflvl34dc03ttrcn25p6j1eg7lmjj89gj0syiox&rid=giphy.gif&ct=g" class="img-thumbnail" alt="...">
+
   </body>
       <?php require_once('footer.php'); ?>
 
